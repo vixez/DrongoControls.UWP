b81ad17421cc3fb29edc097cbc5a7516a3d6746c
Glenn Ruysschaert <vixez@outlook.com>
2018-02-25 11:42:55 +0100

    Testing

---
+++ DrongoControls.UWP/DrongoControls.UWP/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrongoControls.UWP
+{
+    public class TestClass
+    {
+        public static string Testing()
+        {
+            return "string";
+        }
+    }
+}
